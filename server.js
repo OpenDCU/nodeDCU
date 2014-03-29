8c4a95f3d786ec73da928364776006fb75b28865
KimSJ <kim@spencejones.com>
2014-03-29 14:58:36 +0000

    loading page elements from array

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 /*
+
 First experiment with web sockets
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 Licence: GNU GPLv3
@@ -12,20 +13,64 @@ Version  Changes
 
  */
 
+// ****** config options ******
+
+var SERVER = "localhost";
+var PORT   = 3008;
+
+var ELEMENT_SIZE = "70px;";
+
+// ****** namespaces etc. ******
+
 var express = require('express');
 var app = express();
 
 var redis = require('redis').createClient();
 
+var http = require('http').createServer(app);
+var io = require('socket.io');
+
+io.listen(http);
+
+/* redis "DCU:devices" hash table
+  key:    DeviceID
+  value:  { 'contact':  <date/time of last value update>
+          , 'data':     '{JSON stringified}'
+          } 
+*/
+
+var lamp = function (id) {
+  this.content = '<img src="lamp.svg" />'+'<br />'+id;
+};
+
+devices = { "lamp123": new lamp("lamp123")
+          , "lamp124": new lamp("lamp124")
+          };
+
+// ****** build web pages ******
+ 
 // top and tail
 var header = function(req, res, next) {
     res.writeHeader(200, {'content-type': "text/html"});
     res.write(
       '<html>'+
       '<head>'+
-      '<title>Test page</title>'+
+      '  <title>Test page</title>'+
+      '  <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>'+
+      '  <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css" />'+
+      '  <script src="http://' + SERVER + ':' + PORT + '/socket.io/socket.io.js"></script>'+
+      '  <style>'+
+      '    .device {'+
+      '      min-width: '+ ELEMENT_SIZE +
+      '      width: '+ ELEMENT_SIZE +
+      '      min-height: '+ ELEMENT_SIZE +
+      '      height: '+ ELEMENT_SIZE +
+      '      border: 2px solid red;'+
+      '    }'+
+      '  </style>'+
       '</head>'+
-      '<body>'
+      '<body>'+
+      '<h1>Test page</h1>'
     );
 	next();
 };
@@ -48,8 +93,11 @@ var pageMeat = function (req, res, next) {
 		} else if (v===null) {
 			res.write("Page not found");
 		} else {
-			v = JSON.parse(v);
-			res.write('<div>'+v.value+'<br /></div>');
+			v = JSON.parse(v).value;
+      // v is now the array of instances
+      for (var i=0; i<v.length; i++){
+        res.write('<div class="device" id="'+v[i]+'"">' + devices[v[i]].content + '</div>');
+      }
 		}
 		next();
     });
@@ -61,14 +109,20 @@ app.get('/page/:id', header, pageMeat, footer, function(req, res) {
   res.end();
 });
 
-require('http').createServer(app).listen(3008);
+http.listen(PORT);
 
 
 
 
 // ********** socket.io *********
-
-/*var state = 0;
+/*io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+*/
+/* var state = 0;
 
 // initialise console reading
 var readline = require('readline');
@@ -78,6 +132,7 @@ var rl = readline.createInterface({
 			output: process.stdout
 		});
 
+*
 // initialise web socket server
 var WebSocketServer = require('ws').Server,
     wss = new WebSocketServer({port: 1337}),
